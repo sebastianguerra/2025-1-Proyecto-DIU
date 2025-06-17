2e089bb43e6737378fa54657b698257e6a6dcdca
Sebastian Guerra <spguerraespinoza@gmail.com>
2025-06-16 21:10:04 -0400

    feat: add button to enter complaint

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,27 @@
 import Map from "@/components/Map";
+import { Button } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
-    <Map />
+    <>
+      <Map />
+      <Button
+        position="fixed"
+        bottom="100px"
+        left="50%"
+        transform="translateX(-50%)"
+        colorPalette="purple"
+        size="xl"
+        borderRadius={100}
+        px={8}
+        asChild
+      >
+        <Link to="/TipoDenuncia">
+          HACER DENUNCIA
+        </Link>
+      </Button>
+    </>
   );
 }
 
