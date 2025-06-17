ace506e43164fc9ef4cdfb6390fa727a8a8712a6
Sebastian Guerra <spguerraespinoza@gmail.com>
2025-06-16 20:45:44 -0400

    feat: add labels to nav buttons

--- src/components/layout/mobile_navigation.tsx
+++ src/components/layout/mobile_navigation.tsx
@@ -1,6 +1,12 @@
-import { Box, HStack, IconButton } from "@chakra-ui/react";
+import { Box, HStack, Icon, Text, VStack } from "@chakra-ui/react";
 import { LuCircleUserRound, LuOctagonAlert, LuScanEye } from "react-icons/lu";
 import { Link } from "react-router-dom";
+
+const buttons = [
+  { to: "/seguimiento", icon: <LuScanEye />, label: "Seguimiento" },
+  { to: "/", icon: <LuOctagonAlert />, label: "Denunciar" },
+  { to: "/perfil", icon: <LuCircleUserRound />, label: "Perfil" },
+]
 
 function MobileNav() {
   return (
@@ -15,23 +21,26 @@ function MobileNav() {
         padding={4}
       >
         <HStack justify="space-between" w="full">
-          <Link to="/seguimiento">
-            <IconButton colorScheme="teal" variant="ghost" size="lg">
-              <LuScanEye />
-            </IconButton>
-          </Link>
-
-          <Link to="/TipoDenuncia">
-            <IconButton colorScheme="teal" variant="ghost" size="lg">
-              <LuOctagonAlert />
-            </IconButton>
-          </Link>
-
-          <Link to="/perfil">
-            <IconButton colorScheme="teal" variant="ghost" size="lg">
-              <LuCircleUserRound />
-            </IconButton>
-          </Link>
+          {buttons.map((button) => (
+            <Link key={button.label} to={button.to}>
+              <VStack
+                alignItems="center"
+                justifyContent="space-between"
+                color="black"
+                gap={0.1}
+                _hover={{ color: "teal.500" }}
+              >
+                <Icon
+                  aria-label={button.label}
+                  colorScheme="teal"
+                  size="md"
+                >
+                  {button.icon}
+                </Icon>
+                <Text fontSize="xs">{button.label}</Text>
+              </VStack>
+            </Link>
+          ))}
         </HStack>
       </Box>
     </>
